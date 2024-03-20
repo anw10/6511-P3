5bff671bd49264a83f9722c91410b0d5504022c7
Arsalan Bin Najeeb <38084250+anw10@users.noreply.github.com>
2024-03-20 15:08:49 -0400

    api addition

--- tic.py
+++ tic.py
@@ -14,10 +14,32 @@ def terminal_state():
 
 
 #######API##################
+url = "https://www.notexponential.com/aip2pgaming/api/index.php"
 
 
 def send_move():
     print(requests.get())
 
 
+def get_teams():
+    payload = {}
+    params = {"type": "myTeams"}
+    headers = {
+        "x-api-key": "250a442d345be5d375c5",
+        "userid": "2620",
+        "Content-Type": "application/x-www-form-urlencoded",
+        "User-Agent": "PostmanRuntime/7.37.0",
+    }
+
+    response = requests.get(url, headers=headers, data=payload, params=params)
+
+    print(response.text)
+
+
+def get_board():
+    print()
+
+
 #######API##################
+
+get_teams()
