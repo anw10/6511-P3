c2f48752cc76c90a280556062a393b23995e00f5
Arsalan Bin Najeeb <38084250+anw10@users.noreply.github.com>
2024-03-20 14:40:45 -0400

    template

--- tic.py
+++ tic.py
@@ -1,1 +1,23 @@
 import requests
+
+
+def min():
+    print()
+
+
+def max():
+    print()
+
+
+def terminal_state():
+    print()
+
+
+#######API##################
+
+
+def send_move():
+    print(requests.get())
+
+
+#######API##################
