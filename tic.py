1c2fef3fba5a13425942e6191abc78540a1db6ab
Keuntae Kim <ktkim2k1@paran.com>
2024-03-29 06:49:29 -0400

    03/29/2024 - Added all needed API functions in tic.py. Will be updating.

--- tic.py
+++ tic.py
@@ -38,11 +38,112 @@ def min_node(curr_game, state):
     return v, move
 
 
-#######API##################
+#########################################
+#####              API              #####
+#####   - API is case sensitive.    #####
+#####   - e.g. teamid != teamId     #####
+#########################################
+URL = "https://www.notexponential.com/aip2pgaming/api/index.php"
 
+#------- One Time Operations -------#
+def create_team():
+    """
+    Request Type: POST
+    
+    Parameters: type=team, name=$name
+    Return Values: Team ID.  Fails if team already exists, or team name is too short, or too long.
+    """    
+    pass
 
-def send_move(move: tuple[int, int], gameId: str):
+def add_team_member():
+    """
+    Request Type: POST
+    
+    Parameter: type=member, teamId, userId
+    Return Values: OK. Fails if user is already in that team.
+    """
+    pass
 
+def remove_team_member():
+    """
+    Request Type: POST
+    
+    Parameter: type=removeMember, teamId, userId
+    Return Values: OK.
+    """
+    pass
+
+def get_team_member():
+    """
+    Request Type: GET
+    
+    Parameters: type=team, teamId=$teamid
+    Return Values: userids, comma separated
+    """
+    pass
+
+def get_my_teams():
+    """
+    Request Type: GET
+    
+    Parameters: type=myTeams
+    Return Values: teams, comma separated.  Generally, this should be just one.
+
+    https://www.notexponential.com/aip2pgaming/api/index.php?type=myTeams
+    """
+    payload = {}
+    params = {"type": "myTeams"}
+    headers = {
+        "x-api-key": keys.API_KEY,
+        "userid": keys.USER_ID,
+        "Content-Type": "application/x-www-form-urlencoded",
+        "User-Agent": "PostmanRuntime/7.37.0",
+    }
+
+    # response = requests.get(keys.URL, headers=headers, data=payload, params=params)
+    response = requests.get(URL, headers=headers, data=payload, params=params)
+
+    print(response.text)
+
+#------- Playing Games / Ongoing Operations -------#
+def create_game():
+    """
+    Request Type: POST
+    
+    Parameters: 
+    - type=game
+    - teamId1
+    - teamId2
+    - gameType=TTT  // This is the only value supported this semester
+    - Optionally:
+        - boardSize=20
+        - target=10 (Needs to be <= boardSize) 
+        // Default values are 12 and 6
+    Return Values: GameID
+    """
+    pass
+
+def get_my_games():
+    """
+    Request Type: GET
+    
+    Parameters: type=myGames or myOpenGames 
+    Return Values:games, comma separated
+    """
+    pass
+
+def make_move(move: tuple[int, int], gameId: str):
+    """
+    Request Type: POST
+    
+    Parameter: type=move, gameId, teamId, move
+    Return Value: Move ID. 
+    Fails in following cases:
+    - If no such game
+    - If the team is not a participant in the game
+    - If it is not the move of the team making that move
+    - If the move dimensions are negative or >= n.  (Move starts from 0,0.  That is, 0 - indexing]
+    """
     # Pre-process args
     move = str(move).strip("()")
 
@@ -54,48 +155,70 @@ def send_move(move: tuple[int, int], gameId: str):
         "User-Agent": "PostmanRuntime/7.37.0",
     }
 
-    response = requests.post(keys.URL, headers=headers, data=payload)
+    # response = requests.post(keys.URL, headers=headers, data=payload)
+    response = requests.post(URL, headers=headers, data=payload)
     print(response.text)
 
+def get_moves():
+    """
+    Request Type: GET
+    
+    Parameters: type=moves, gameId, Count of most recent of moves
+    Return Values: List of Moves, comma separated
+    """
+    pass
 
-def get_teams():
-    payload = {}
-    params = {"type": "myTeams"}
-    headers = {
-        "x-api-key": keys.API_KEY,
-        "userid": keys.USER_ID,
-        "Content-Type": "application/x-www-form-urlencoded",
-        "User-Agent": "PostmanRuntime/7.37.0",
-    }
+def get_game_details():
+    """
+    Request Type: GET
+    
+    Parameters: type=gameDetails, gameId
+    Return Values: Game Details as JSON
+    """
+    pass
 
-    response = requests.get(keys.URL, headers=headers, data=payload, params=params)
+def get_board_string():
+    """
+    Request Type: GET
+    
+    Parameters: type=boardString, gameId
+    Return Values: Board, in form of a string of O,X,-
+    """
+    pass
+    
+def get_board_map():
+    """
+    Request Type: GET
+    
+    Parameters: type=boardMap, gameId
+    Return Values: Board, in form of a string of O,X,-
+    """
+    pass
 
-    print(response.text)
+# def get_board():
+#     payload = {}
+#     params = {"type": "boardMap", "gameId": "4671"}
+#     headers = {
+#         "x-api-key": keys.API_KEY,
+#         "userid": keys.USER_ID,
+#         "Content-Type": "application/x-www-form-urlencoded",
+#         "User-Agent": "PostmanRuntime/7.37.0",
+#     }
+
+#     # response = requests.get(keys.URL, headers=headers, params=params)
+#     response = requests.get(URL, headers=headers, params=params)
+
+#     json_board = json.loads(response.text)["output"]
+
+#     if json_board == None:
+#         print("Board is empty")
+#     else:
+#         print(json_board)
 
 
-def get_board():
-    payload = {}
-    params = {"type": "boardMap", "gameId": "4671"}
-    headers = {
-        "x-api-key": keys.API_KEY,
-        "userid": keys.USER_ID,
-        "Content-Type": "application/x-www-form-urlencoded",
-        "User-Agent": "PostmanRuntime/7.37.0",
-    }
+################## for Testing ################## #TODO: cleanup when it's done
 
-    response = requests.get(keys.URL, headers=headers, params=params)
-
-    json_board = json.loads(response.text)["output"]
-
-    if json_board == None:
-        print("Board is empty")
-    else:
-        print(json_board)
-
-
-#######API##################
-
-# get_teams()
+# get_my_teams()
 # get_board()
 
-# send_move(move=(1, 5), gameId="4671")
+# make_move(move=(1, 5), gameId="4671")
